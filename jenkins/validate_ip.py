99b74487bc1de66950118f9173310f0b85603096
nickthejagger <nichola.indra@gmail.com>
2023-12-03 23:21:49 +0700

    chore: init project

---
+++ jenkins/validate_ip.py
@@ -0,0 +1,18 @@
+import sys
+import ipaddress
+
+if (args_count := len(sys.argv)) > 2:
+    print(f"One argument expected, got {args_count - 1}")
+    raise SystemExit(2)
+elif args_count < 2:
+    print("You must insert the ip number")
+    raise SystemExit(2)
+
+def validate_ip(ip_string):
+   try:
+       is_ip_valid = ipaddress.ip_address(ip_string)
+       print("The IP address '{is_ip_valid}' is valid.")
+   except ValueError:
+       print("The IP address '{is_ip_valid}' is not valid")
+
+validate_ip(sys.argv[1])
